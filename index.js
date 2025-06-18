26b2d4e1a6ba9a4e0784bf6c564bcde4de0329b5
manjukolkar <manjunathkolkar2019@gmail.com>
2025-06-18 15:43:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send(`
     <!DOCTYPE html>
@@ -30,4 +34,8 @@ app.get('/', (req, res) => {
     </body>
     </html>
   `);
+});
+
+app.listen(port, () => {
+  console.log(`✅ Server is running on port ${port}`);
 });
